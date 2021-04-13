7d3349bd6b393052142bf74342f1134cabd56c50
Luis Juarez <luismanueljf@gmail.com>
2021-04-12 22:09:51 -0500

    initial commit

---
+++ ios/RNTrackPlayer/RNTrackPlayerBridge.h
@@ -0,0 +1,13 @@
+//
+//  RNTrackPlayerBridge.h
+//  RNTrackPlayerBridge
+//
+//  Created by David Chavez on 7/1/17.
+//  Copyright © 2017 David Chavez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTConvert.h>
+
+@interface RNTrackPlayerBridge: NSObject
+@end
